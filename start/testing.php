621a51370cab933e917de0c6d8b9535b063b2e06
zhanat <iskakov_zhanat@mail.ru>
2017-08-15 21:28:39 +0600

    lesson 1

---
+++ start/testing.php
@@ -0,0 +1,13 @@
+<?php
+require __DIR__.'/vendor/autoload.php';
+
+$client = new \GuzzleHttp\Client([
+    'base_url' => 'http://localhost:8000',
+    'defaults' => [
+        'exceptions' => false
+    ]
+]);
+
+$response = $client->post('/api/programmers');
+echo $response;
+echo "\n\n";
