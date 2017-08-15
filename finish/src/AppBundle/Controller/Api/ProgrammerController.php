e9ca506754a931a4a8ea8d7ae558eefaa7da573d
zhanat <iskakov_zhanat@mail.ru>
2017-08-15 20:54:48 +0600

    course src

---
+++ finish/src/AppBundle/Controller/Api/ProgrammerController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace AppBundle\Controller\Api;
+
+use AppBundle\Controller\BaseController;
+use AppBundle\Entity\Programmer;
+use AppBundle\Form\ProgrammerType;
+use AppBundle\Form\UpdateProgrammerType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProgrammerController extends BaseController
+{
+    /**
+     * @Route("/api/programmers")
+     * @Method("POST")
+     */
+    public function newAction(Request $request)
+    {
+        $programmer = new Programmer();
+        $form = $this->createForm(new ProgrammerType(), $programmer);
+        $this->processForm($request, $form);
+
+        $programmer->setUser($this->findUserByUsername('weaverryan'));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($programmer);
+        $em->flush();
+
+        $response = $this->createApiResponse($programmer, 201);
+        $programmerUrl = $this->generateUrl(
+            'api_programmers_show',
+            ['nickname' => $programmer->getNickname()]
+        );
+        $response->headers->set('Location', $programmerUrl);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/api/programmers/{nickname}", name="api_programmers_show")
+     * @Method("GET")
+     */
+    public function showAction($nickname)
+    {
+        $programmer = $this->getDoctrine()
+            ->getRepository('AppBundle:Programmer')
+            ->findOneByNickname($nickname);
+
+        if (!$programmer) {
+            throw $this->createNotFoundException(sprintf(
+                'No programmer found with nickname "%s"',
+                $nickname
+            ));
+        }
+
+        $response = $this->createApiResponse($programmer, 200);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/api/programmers")
+     * @Method("GET")
+     */
+    public function listAction()
+    {
+        $programmers = $this->getDoctrine()
+            ->getRepository('AppBundle:Programmer')
+            ->findAll();
+
+        $response = $this->createApiResponse(['programmers' => $programmers], 200);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/api/programmers/{nickname}")
+     * @Method({"PUT", "PATCH"})
+     */
+    public function updateAction($nickname, Request $request)
+    {
+        $programmer = $this->getDoctrine()
+            ->getRepository('AppBundle:Programmer')
+            ->findOneByNickname($nickname);
+
+        if (!$programmer) {
+            throw $this->createNotFoundException(sprintf(
+                'No programmer found with nickname "%s"',
+                $nickname
+            ));
+        }
+
+        $form = $this->createForm(new UpdateProgrammerType(), $programmer);
+        $this->processForm($request, $form);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($programmer);
+        $em->flush();
+
+        $response = $this->createApiResponse($programmer, 200);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/api/programmers/{nickname}")
+     * @Method("DELETE")
+     */
+    public function deleteAction($nickname)
+    {
+        $programmer = $this->getDoctrine()
+            ->getRepository('AppBundle:Programmer')
+            ->findOneByNickname($nickname);
+
+        if ($programmer) {
+            // debated point: should we 404 on an unknown nickname?
+            // or should we just return a nice 204 in all cases?
+            // we're doing the latter
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($programmer);
+            $em->flush();
+        }
+
+        return new Response(null, 204);
+    }
+
+    private function processForm(Request $request, FormInterface $form)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $clearMissing = $request->getMethod() != 'PATCH';
+        $form->submit($data, $clearMissing);
+    }
+}
