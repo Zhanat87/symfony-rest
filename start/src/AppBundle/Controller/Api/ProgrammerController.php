c4a2f8ea49410d24165d5eb0dc62614731c94574
zhanat <iskakov_zhanat@mail.ru>
2017-08-16 08:33:55 +0600

    lesson 2

--- start/src/AppBundle/Controller/Api/ProgrammerController.php
+++ start/src/AppBundle/Controller/Api/ProgrammerController.php
@@ -2,19 +2,30 @@
 
 namespace AppBundle\Controller\Api;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Controller\BaseController;
+use AppBundle\Entity\Programmer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class ProgrammerController extends Controller
+class ProgrammerController extends BaseController
 {
     /**
      * @Route("/api/programmers")
      * @Method("POST")
      */
-    public function newAction()
+    public function newAction(Request $request)
     {
-        return new Response('Let\'s do this!');
+        $data = json_decode($request->getContent(), true);
+        $programmer = new Programmer($data['nickname'], $data['avatarNumber']);
+        $programmer->setTagLine($data['tagLine']);
+        $programmer->setUser($this->findUserByUsername('weaverryan'));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($programmer);
+        $em->flush();
+
+        return new Response('It worked. Believe me - I\'m an API');
     }
 }
